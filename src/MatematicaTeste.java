bfd83b4d56f4706d75e32c07fcf09b589f2d1caf
unknown <prixmagalha@gmail.com>
2022-06-13 16:00:05 -0300

    Iniciando meu repositorio

---
+++ src/MatematicaTeste.java
@@ -0,0 +1,17 @@
+
+public class MatematicaTeste {
+
+	public static void main(String[] args) {
+		
+		Matematica m = new Matematica();
+		
+		int ma = m.maior(10, 20);
+		System.out.println(ma);
+		
+		
+		double so = m.soma(10, 20);
+		System.out.println (so);
+
+	}
+
+}
